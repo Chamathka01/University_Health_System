cf0c97a0543c34c76ac6ee98560e16a7d9478d32
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-05-26 10:41:36 +0530

    Created StudentController

--- UHS/app/Http/Controllers/StudentController.php
+++ UHS/app/Http/Controllers/StudentController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Models\Visit;
 
 class StudentController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        $student = Session::get('user');
+
+        $visits = Visit::with('medicalRecord')
+                    ->where('student_id', $student->id)
+                    ->orderBy('visit_date', 'desc')
+                    ->get();
+
+        return view('student.dashboard', compact('visits'));
+    }
 }
