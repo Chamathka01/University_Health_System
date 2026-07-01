9bc649fcf245de16a9364a9bba99354082090aaf
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-07-01 09:40:42 +0530

    Build the Medicine Eloquent Model

---
+++ UHS/app/Models/Medicine.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medicine extends Model
+{
+    use HasFactory;
+
+    protected $table = 'medicines';
+
+    protected $fillable = [
+        'name',
+        'batch_number',
+        'stock_quantity',
+        'min_required_alert',
+        'expiry_date',
+    ];
+}
